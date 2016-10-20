937f91e4a43d87794e9e69bf6bbe8f50513e963b
Kristian Wilson <kristian.wilson@rgroup.co.uk>
2016-10-20 23:10:48 +0100

    Initial setup ready for development.

---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const stylelint = require('stylelint');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+    // Entry accepts a path or an object of entries. We'll be using the
+    // latter form given it's convenient with more complex configurations
+    
+    entry: {
+        app: PATHS.app
+    },
+    // Add resolve.extensions
+    // '' is needed to allow imports without an extensions
+    // Note the .'s before extensions as it will fail to match without!!!
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['eslint'],
+                include: PATHS.app
+            },
+            {
+                test: /\.css$/,
+                loaders: ['postcss'],
+                include: PATHS.app
+            },           
+        ],
+        loaders: [
+            {
+                // Test expects RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
+            },
+            // Set up jsx. This accepts js too thanks to RegExp
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
+                include: PATHS.app
+            }
+        ]
+    },
+    postcss: function () {
+        return [stylelint({
+            rules: {
+                'color-hex-case': 'lower'
+            }
+        })]
+    }
+};
+
+//Default configuration
+if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            contentBase: PATHS.build,
+
+            // Enable history API fallback sp HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: true,
+            hot: true,
+            inline : true,
+            progress: true,
+
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
+
+            // Parse host and port from env so this is easy to customise
+            //
+            // If you use Vagrant or Cloud9, set
+            // host: process.env.HOST || '0.0.0.0';
+            //
+            // 0.0.0.0 is available to all network devices unlike default
+            // localhost
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new NpmInstallPlugin({
+                save: true // --save
+            })
+        ]
+    });
+}
+
+if(TARGET === 'build') {
+    module.exports = merge(common, {});
+}
