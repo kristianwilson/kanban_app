c799e079b7993ab89d7653a19aa17f9ba5f6461b
Kristian Wilson <kristian.wilson@rgroup.co.uk>
2016-10-24 20:25:57 +0100

    Completed Chapter 8
    Configured build

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,19 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const stylelint = require('stylelint');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// Load *package.json* so we can use `dependencies` from there
+const pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build')
+    build: path.join(__dirname, 'build'),
+    style: path.join(__dirname, 'app/main.css')
 };
+
 
 process.env.BABEL_ENV = TARGET;
 
@@ -17,7 +24,8 @@ const common = {
     // latter form given it's convenient with more complex configurations
     
     entry: {
-        app: PATHS.app
+        app: PATHS.app,
+        style: PATHS.style
     },
     // Add resolve.extensions
     // '' is needed to allow imports without an extensions
@@ -27,7 +35,9 @@ const common = {
     },
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        // Output using entry name
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[chunkhash].js'
     },
     module: {
         preLoaders: [
@@ -43,13 +53,6 @@ const common = {
             },           
         ],
         loaders: [
-            {
-                // Test expects RegExp! Note the slashes!
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                // Include accepts either a path or an array of paths.
-                include: PATHS.app
-            },
             // Set up jsx. This accepts js too thanks to RegExp
             {
                 test: /\.jsx?$/,
@@ -64,7 +67,15 @@ const common = {
                 'color-hex-case': 'lower'
             }
         })]
-    }
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: 'node_modules/html-webpack-template/index.ejs',
+            title: 'Kanban app',
+            appMountId: 'app',
+            inject: false
+        })
+    ]
 };
 
 //Default configuration
@@ -72,7 +83,6 @@ if(TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
         devtool: 'eval-source-map',
         devServer: {
-            contentBase: PATHS.build,
 
             // Enable history API fallback sp HTML5 History API based
             // routing works. This is a good default that will come
@@ -95,6 +105,16 @@ if(TARGET === 'start' || !TARGET) {
             host: process.env.HOST,
             port: process.env.PORT
         },
+        module : {
+            loaders: [
+                // Define development specific CSS setup
+                {
+                    test: /\.css$/,
+                    loaders: ['style', 'css'],
+                    include: PATHS.app
+                }
+            ]
+        },
         plugins: [
             new webpack.HotModuleReplacementPlugin(),
             new NpmInstallPlugin({
@@ -104,6 +124,49 @@ if(TARGET === 'start' || !TARGET) {
     });
 }
 
-if(TARGET === 'build') {
-    module.exports = merge(common, {});
+if(TARGET === 'build' || TARGET === 'stats') {
+    module.exports = merge(common, {
+        // Define vendor entry point needed for splitting
+        entry: {
+            vendor: Object.keys(pkg.dependencies).filter(function(v) {
+                // Exclude alt-utils as it won't work with this setup
+                // due to the way the package has been designed
+                // (no package.json main)
+                return v !== 'alt-utils';
+            })
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css'),
+                    include: PATHS.app,
+                }
+            ]
+        },
+        plugins: [
+            new CleanPlugin(PATHS.build),
+            // Ouput extracted CSS to a filename
+            new ExtractTextPlugin('[name].[chunkhash].css'),
+            // Extract vendor and manifest files
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest']
+            }),
+            // Settings DefinePlugin affects React library size!
+            // DefinePlugin replaces content "as is" so we need some extra quotes
+            // for the generated code to make sense
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+                
+                // You can set this to JSON.stringify('development') for your
+                // development target to force NODE_ENV to development mode
+                // no matter what
+            }),            
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    });
 }
